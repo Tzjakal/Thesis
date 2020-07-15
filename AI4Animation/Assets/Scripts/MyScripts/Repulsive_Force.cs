22d0dcfa09b27f1aa71b6835d590c81b2f779091
Tzjakal <67586308+Tzjakal@users.noreply.github.com>
2020-07-15 16:41:38 +0300

    Collision Avoidance (spherical coordinates)_1

---
+++ AI4Animation/Assets/Scripts/MyScripts/Repulsive_Force.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Repulsive_Force : MonoBehaviour
+{
+    private GameObject target;
+    private float d;
+    private float r;
+    private float th;
+    private float f;
+    
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("IKtarget");
+        d = Mathf.Sqrt(Vector3.SqrMagnitude(transform.position - target.transform.position));
+
+
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = transform.position.z;
+
+        d = Mathf.Sqrt(Vector3.SqrMagnitude(transform.position - target.transform.position));
+
+        r = Mathf.Sqrt(x * x + y * y + z * z);
+        th = Mathf.Atan2(Mathf.Sqrt(x * x + y * y), z);
+        f = Mathf.Atan2(y, x);
+
+        target.transform.position = (new Vector3(r * Mathf.Sin(th) * Mathf.Cos(f), r * Mathf.Sin(th) * Mathf.Sin(f), r * Mathf.Cos(th)));
+    } 
+}
